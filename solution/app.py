13f4d0f7a93ffd9a394d74f0f709c4d40b66727d
Kevin Du <kevin_du@brown.edu>
2020-01-17 15:00:33 -0500

    Added migrations to solution

--- solution/app.py
+++ solution/app.py
@@ -1,13 +1,15 @@
 from flask import Flask, render_template, request, redirect, url_for 
-from flask_sqlalchemy import SQLAlchemy  
+from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
   
 app = Flask(__name__) 
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///todo.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
   
-db = SQLAlchemy(app) 
-  
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
 class Todo(db.Model): 
     id = db.Column(db.Integer, primary_key = True) 
     text = db.Column(db.String(200)) 
